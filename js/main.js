8d8f56f06acaa3b801d590c6b110c32174ff1e92
unknown <gabrieltorresmora2@gmail.com>
2022-09-16 11:22:40 -0300

    haciendo pruebas y mejoras en la pokeApi

--- js/main.js
+++ js/main.js
@@ -19,37 +19,49 @@ function createPokemons(pokemons){
     const card = document.createElement('div')
     card.classList.add('pokemon-block');
 
+    // CREANDO ELEMENTOS DEL DOM
     const imagenContainer = document.createElement('div'),
           imagen = document.createElement('img'),
           titulo = document.createElement('h2'),
           number = document.createElement('p'),
-          baseExperiencia = document.createElement('p');
-          
+          baseExperiencia = document.createElement('p'),
+          peso = document.createElement('p');
          
-
-        
+          
+    // DANDOLE CLASES A LOS ELEMETOS CREADOS EN EL DOM
         titulo.classList.add('titulo')  
         number.classList.add('number')
+        peso.classList.add('peso-pokemon')
+        baseExperiencia.classList.add('experiencia-pokemons')
         imagenContainer.classList.add('contenedor-imagen')  
         imagen.classList.add('imagen');
 
-          
-          baseExperiencia.textContent = pokemons.base_experience;
-          titulo.textContent = pokemons.name
-          number.textContent = `#${pokemons.id.toString().padStart(3, 0)}`;
-          imagen.src = pokemons.sprites.other.home.front_default;
 
+    // ANINANDO LOS ELEMENTOS DEL DOM CON APPECHILD PARA QUE ASI APAREZCA EL CONTENIDO      
+          baseExperiencia.textContent = `Experiencia: ${pokemons.base_experience}`;
+          titulo.textContent = pokemons.name
+          number.textContent = `Tarjeta: #${pokemons.id.toString().padStart(3, 0)}`;
+          imagen.src = pokemons.sprites.other.home.front_default;
+          peso.textContent = `Peso: ${pokemons.weight}`
+         
+    
           imagenContainer.appendChild(imagen);
 
           card.appendChild(imagenContainer);
           card.appendChild(titulo);
           card.appendChild(number);
+          card.appendChild(peso)
           card.appendChild(baseExperiencia)
-    
+
+
+          // HACIENDO UN ARRAY PARA LOS STAT DE LOS POKEMONS
 
           pokemonContainer.appendChild(card);
 }
-pokemons(500)
+pokemons(9);
+
+
+
 
 
 
